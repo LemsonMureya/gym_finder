9a142e10ba2120b38571dd941a185584aae1bd38
Lemson Mureya <lmureya@conncoll.edu>
2024-03-13 01:02:26 -0400

    Added map view and search by location functionality and fixed variables

--- gym_platform/settings.py
+++ gym_platform/settings.py
@@ -79,20 +79,13 @@ WSGI_APPLICATION = 'gym_platform.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'NAME': 'gymfindr',
-        'USER': 'lemson',
-        'PASSWORD': 'test_password',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': 'localhost',
         'PORT': '5432',
     }
